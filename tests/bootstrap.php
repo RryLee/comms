185241a7aad5465efd9b01a63660c4778e1b687f
Rowan Lewis <rl@nbsp.io>
2014-07-08 23:46:11 +0200

    Use the project autoloader if no local composer.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,8 +1,14 @@
 <?php
 
-$loader = (require __DIR__ . '/../vendor/autoload.php');
-$namespaces = $loader->getPrefixesPsr4();
+if (is_file(__DIR__ . '/../vendor/autoload.php')) {
+    $loader = (require __DIR__ . '/../vendor/autoload.php');
+    $namespaces = $loader->getPrefixesPsr4();
 
-foreach ($namespaces as $namespace => $dir) {
-    $loader->addPsr4($namespace . 'Tests\\', __DIR__);
+    foreach ($namespaces as $namespace => $dir) {
+        $loader->addPsr4($namespace . 'Tests\\', __DIR__);
+    }
+}
+
+else {
+    require __DIR__ . '/../../autoload.php';
 }
